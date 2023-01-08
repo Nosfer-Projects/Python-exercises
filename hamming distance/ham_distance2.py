4535911494dadd834fc6a303e1a323383704569f
Nosfer <nosfer.python@gmail.com>
2023-01-08 18:02:54 +0100

    adding task

--- hamming distance/ham_distance2.py
+++ hamming distance/ham_distance2.py
@@ -1,4 +1,5 @@
-
+# We will define the weight of a vector u as the Hamming distance of this vector from the zero vector. So in the case of vector '1001001', its weight will be equal to the Hamming distance of vector '1001001' from vector '0000000'.
+# Implement a function called hamming_weight() that will return the weight of the vector determined as above. In the solution, you can use the hamming_distance() function. It is also worth noting a slightly simpler implementation. The weight of the vector will be equal to the number of 1's in the vector.
 
 def hamming_distance(num1, num2):
     count = 0
