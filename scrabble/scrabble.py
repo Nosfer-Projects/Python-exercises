5adacabd8ce5274a28b1e6cfa5a1b0ecf2911161
Nosfer <nosfer.python@gmail.com>
2023-01-08 18:31:53 +0100

    adding task

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,4 +1,22 @@
+# Below is the score set for the English version:
 
+# empty tile - 0 points (usually two in a set)
+
+# EAIONRTLSU - 1 pt
+
+# DG - 2 points
+
+# BCMP - 3 pts
+
+# FHVWY - 4 pts
+
+# K - 5 pts
+
+# JX - 8 pts
+
+# QZ - 10 pts
+
+# Implement a function called score() that will return the score for the word you composed. We assume that the given word is grammatically correct. For simplicity, an empty tile can be represented as a space character ' '.
 
 
 def score(main_string):
