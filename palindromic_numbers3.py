8bd1dbcdd9bbdccc325c579fb9ad4eebfaf10901
Nosfer <nosfer.python@gmail.com>
2023-01-08 14:09:22 +0100

    fix some of code

--- palindromic_numbers3.py
+++ palindromic_numbers3.py
@@ -1,8 +1,10 @@
 # Implement a function called is_palindrome() that checks whether the given number is palindromic in both decimal and binary notation.
 
 def is_palindrome(number):
-    if str(number) == str(number)[::-1]and str(bin(number)) == str(bin(number)):
-        return True
+    if str(number) == str(number)[::-1]:
+        bin_number  = str(bin(number))[2:]
+        if bin_number == bin_number[::-1]:
+            return True
     else:
         return False
 
