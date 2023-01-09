a359e0ca6b2e4598f3f88f2753c96d21e169bd07
Nosfer <nosfer.python@gmail.com>
2023-01-09 13:57:14 +0100

    improve solution code

--- SQLite/sql3.py
+++ SQLite/sql3.py
@@ -8,25 +8,27 @@
 import sqlite3
 
 
-def executescript():
-    conn = sqlite3.connect("app.db")
-    cur= conn.cursor()
-    
-    sql = '''CREATE TABLE IF NOT EXISTS customer (
+import sqlite3
+ 
+ 
+conn = sqlite3.connect('app.db')
+cur = conn.cursor()
+ 
+cur.execute('''CREATE TABLE IF NOT EXISTS customer (
     customer_id INTEGER PRIMARY KEY,
     first_name  TEXT,
     last_name   TEXT,
     email       TEXT
-            )'''
-    cur.execute(sql)
-    cur.execute('''INSERT INTO customer (first_name, last_name, email)
-                VALUES ('John', 'Smith', 'john.smith@esmartdata.org')''')
-    cur.execute('''INSERT INTO customer (first_name, last_name, email)
-                VALUES ('Joe', 'Doe', 'joe.doe@esmartdata.org')''')
-    cur.execute('''INSERT INTO customer (first_name, last_name, email)
-                VALUES ('Mike', 'Smith', 'mike.smith@esmartdata.org') ''')
-    conn.commit()
-    cur.close()
-    conn.close()
-
-executescript()
+)''')
+ 
+cur.executescript('''INSERT INTO customer (first_name, last_name, email)
+VALUES ('John', 'Smith', 'john.smith@esmartdata.org');
+ 
+INSERT INTO customer (first_name, last_name, email)
+VALUES ('Joe', 'Doe', 'joe.doe@esmartdata.org');
+ 
+INSERT INTO customer (first_name, last_name, email)
+VALUES ('Mike', 'Smith', 'mike.smith@esmartdata.org');''')
+ 
+conn.commit()
+conn.close()
