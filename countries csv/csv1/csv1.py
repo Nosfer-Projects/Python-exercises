ce569ab36581d77173a42c6037579989103a1232
Nosfer <nosfer.python@gmail.com>
2023-01-10 10:38:09 +0100

    change folders

---
+++ countries csv/csv1/csv1.py
@@ -0,0 +1,16 @@
+# Load the Customer.csv file and extract all the unique country names sorted alphabetically into a list and assign it to the unique_countries variable. Print the contents of the unique_countries variable to the console.
+
+
+
+
+
+
+from itertools import chain
+import csv
+
+with open("Customer.csv", 'r') as file:
+    reader = csv.reader(file)
+    data = list(chain.from_iterable(reader))
+
+unique_countries= list(set(data))
+print(sorted(unique_countries))
