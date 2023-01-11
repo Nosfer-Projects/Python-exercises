4703ccdb60aa4206574a1c1933e9adce5f396c52
Nosfer <nosfer.python@gmail.com>
2023-01-11 13:08:23 +0100

    task and solution

---
+++ binary system/bin4.py
@@ -0,0 +1,80 @@
+# Implement a function called bitwise_xor() that takes two integers as arguments (in decimal) and returns the result of the XOR operation in decimal. You can use int() to convert a number from binary to decimal. In case any given argument is less than zero, raise a ValueError with the message:
+# 'Both numbers must be positive.'
+
+def decimal_to_binary(number):
+    if number == 0:
+        return '0'
+    result = ''
+    while number > 0:
+        result += str(number % 2)
+        number = number // 2
+    return result[::-1]
+
+def bitwise_and(a, b):
+    if a < 0 or b < 0:
+        raise ValueError('Both numbers must be positive.')
+ 
+    a_bin = decimal_to_binary(a)
+    b_bin = decimal_to_binary(b)
+ 
+    max_length = max(len(a_bin), len(b_bin))
+ 
+    a_zfill = a_bin.zfill(max_length)
+    b_zfill = b_bin.zfill(max_length)
+ 
+    binary_and = [
+        str(int(char_a == '1' and char_b == '1'))
+        for char_a, char_b in zip(a_zfill, b_zfill)
+    ]
+ 
+    binary_result = ''.join(binary_and)
+    decimal_result = int(binary_result, base=2)
+ 
+    return decimal_result
+    
+def bitwise_or(a, b):
+    if a < 0 or b < 0:
+        raise ValueError('Both numbers must be positive.')
+ 
+    a_bin = decimal_to_binary(a)
+    b_bin = decimal_to_binary(b)
+ 
+    max_length = max(len(a_bin), len(b_bin))
+ 
+    a_zfill = a_bin.zfill(max_length)
+    b_zfill = b_bin.zfill(max_length)
+ 
+    binary_or = [
+        str(int(char_a == '1' or char_b == '1'))
+        for char_a, char_b in zip(a_zfill, b_zfill)
+    ]
+ 
+    binary_result = ''.join(binary_or)
+    decimal_result = int(binary_result, base=2)
+ 
+    return decimal_result
+
+def bitwise_xor(a, b):
+    if a < 0 or b < 0:
+        raise ValueError('Both numbers must be positive.')
+ 
+    a_bin = decimal_to_binary(a)
+    b_bin = decimal_to_binary(b)
+ 
+    max_length = max(len(a_bin), len(b_bin))
+ 
+    a_zfill = a_bin.zfill(max_length)
+    b_zfill = b_bin.zfill(max_length)
+ 
+    binary_xor = [
+        str(int(char_a != char_b))
+        for char_a, char_b in zip(a_zfill, b_zfill)
+    ]
+ 
+    binary_result = ''.join(binary_xor)
+    decimal_result = int(binary_result, base=2)
+ 
+    return decimal_result
+
+
+print(bitwise_xor(10, 140))
