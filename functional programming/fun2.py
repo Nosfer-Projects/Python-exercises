5a5d2a0612331b244733b3cc6516fc9c439f1bc2
Nosfer <nosfer.python@gmail.com>
2023-01-11 13:41:41 +0100

    task and solution

---
+++ functional programming/fun2.py
@@ -0,0 +1,21 @@
+# In summary, we reduce an iterable object to some final value using a reducing function. There are three steps in this process:
+# Calling the function on the first two elements of the iterable and generating a partial result
+# Calling a function on a partial result and the next element of an iterable object
+# Repeating the process until the elements in the iterable are exhausted and returning the result
+# Convert the reduce() function from the previous exercise into a function called reduce_with_start() so that it takes a third argument called start, which will be the starting value in the iterable reduction process.
+
+
+
+def reduce_with_start(function, iterable, start):
+    if len(iterable) == 0:
+        if start == None:
+            return None
+        else:
+            return start
+    result = start + iterable[0]
+    for item in iterable[1:]:
+        result = function(result, item)
+    return result
+
+
+print(reduce_with_start(lambda x, y: x + y, [], 100))
