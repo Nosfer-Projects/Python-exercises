f0a95c8298fbd3b20b3d34f904dc4c96fb88a366
Nosfer <nosfer.python@gmail.com>
2023-01-12 20:55:36 +0100

    task and solution

---
+++ linear algebra/lin_al4.py
@@ -0,0 +1,76 @@
+# A fragment of a class called Matrix has been implemented, which will provide an interface for working with matrices.
+# Add a method called zero() to the Matrix class that will return a zero matrix of the same size (a matrix filled with all zeros).
+
+class Matrix:
+    def __init__(self, array):
+
+        if not isinstance(array, list):
+            raise TypeError(
+                'To create a matrix you need to pass a nested '
+                'list of values.'
+            )
+        if len(array) != 0:
+            if not all(isinstance(row, list) for row in array):
+                raise TypeError(
+                    'Each element of the array (nested list) must '
+                    'be a list.'
+                )
+            if not all(len(row) for row in array):
+                raise TypeError(
+                    'Columns must contain at least one item.'
+                )
+            column_length = len(array[0])
+
+            if not all(
+                len(row) == column_length for row in array
+            ):
+                raise TypeError(
+                    'All columns must be the same length.'
+                )
+            if not all(
+                type(number) in (int, float)
+                for row in array
+                for number in row
+            ):
+                raise TypeError(
+                    'The values must be of type int or float.'
+                )
+            self.array = array
+        else:
+            self.array = []
+
+    def __repr__(self):
+        return str(self.array)
+
+    @property
+    def n_rows(self):
+        return len(self.array)
+
+    @property
+    def n_cols(self):
+        if len(self.array) == 0:
+            return 0
+        return len(self.array[0])
+
+    @property
+    def size(self):
+        return self.n_rows, self.n_cols
+        
+    @property
+    def is_square_matrix(self):
+        if self.n_cols == self.n_rows:
+            return True
+        else:
+            return False
+
+    def zero(self):
+        array = [
+            [0 for _ in range(self.n_cols)]
+            for _ in range(self.n_rows)
+        ]
+        return Matrix(array)
+
+
+m = Matrix([[3, 1, 6], [5, 2, 6], [5, 2, 6]])
+
+print(m.zero())
