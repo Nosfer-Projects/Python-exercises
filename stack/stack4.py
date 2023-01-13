39859b2899c4d0f1f4232e47f701669afc9a80b5
Nosfer <nosfer.python@gmail.com>
2023-01-13 21:51:43 +0100

    task and solution

--- stack/stack4.py
+++ stack/stack4.py
@@ -30,3 +30,21 @@ class Stack:
         if self.is_empty():
             raise EmptyStackError('The stack is empty.')
         return self._data[-1]
+
+    def is_valid_expression(expression):
+        left_side = '([{'
+        right_side = ')]}'
+    
+        stack = Stack()
+    
+        for char in expression:
+            if char in left_side:
+                stack.push(char)
+            elif char in right_side:
+                if stack.is_empty():
+                    return False
+                if right_side.index(char) != left_side.index(stack.pop()):
+                    return False
+        return stack.is_empty()
+                    
+
