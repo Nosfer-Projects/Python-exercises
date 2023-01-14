95947af503345381e7aca455f138b0520b6da214
Nosfer <nosfer.python@gmail.com>
2023-01-14 18:58:21 +0100

    task and solution

---
+++ queue/queue2.py
@@ -0,0 +1,28 @@
+# Add two methods to the Queue class:
+# method called is_empty(), which will check if the queue is empty. In the case of an empty queue, the method is to return the logical value True, otherwise False
+# a method called first(), which will allow you to read the first element of the queue (without removing it)
+# Also modify the dequeue() method using the implemented is_empty() method in the defined conditional statement.
+
+class Queue:
+    """The simplest queue."""
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def enqueue(self, item):
+        self._data.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError('The queue is empty.')
+        return self._data.pop(0)
+    
+    def is_empty(self):
+        if len(self._data) == 0:
+            return True
+        return False
+    def first(self):
+        return self._data[0]
