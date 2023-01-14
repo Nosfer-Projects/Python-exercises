d3f59cfe7490ded926b987fef0d575b462000221
Nosfer <nosfer.python@gmail.com>
2023-01-14 18:41:26 +0100

    task and solution

---
+++ stack/stack8.py
@@ -0,0 +1,35 @@
+# A class called Stack is implemented, which is the simplest representation of a stack.
+# Add a method called clear() to the Stack class to clear the stack (remove all items from the stack).
+
+class EmptyStackError(Exception):
+    pass
+
+
+class Stack:
+    """The simplest stack."""
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def push(self, item):
+        self._data.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise EmptyStackError('The stack is empty.')
+        return self._data.pop()
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def top(self):
+        if self.is_empty():
+            raise EmptyStackError('The stack is empty.')
+        return self._data[-1]
+    
+    def clear(self):
+        self._data = []
+        
