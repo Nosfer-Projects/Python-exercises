71981d8298670088a11bf81e0760deac0d67e8c6
Nosfer <nosfer.python@gmail.com>
2023-01-22 17:14:52 +0100

    task and solution

---
+++ tests/t4.py
@@ -0,0 +1,13 @@
+# The following area() function is given, which returns the area of ​​a rectangle (without validating the arguments).
+# def area(width, height):
+#     return width * height
+# Assert the following function calls:
+# area(4, 10)
+# area(5, 6)
+
+def area(width, height):
+    return width * height
+ 
+ 
+assert area(4, 10) == 40
+assert area(5, 6) == 30
