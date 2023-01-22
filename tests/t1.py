5fd71b0b347f8a9719f6578dda0e272169f9bd06
Nosfer <nosfer.python@gmail.com>
2023-01-22 17:01:20 +0100

    task and solution

---
+++ tests/t1.py
@@ -0,0 +1,9 @@
+# Using the assert statement, assert the is_canada variable:
+# countries = ['POL', 'ENG', 'GER', 'USA', 'ITA']
+# is_canada = 'CAN' in countries
+# Expected Action: Raising AssertionError.
+
+countries = ['POL', 'ENG', 'GER', 'USA', 'ITA']
+is_canada = 'CAN' in countries
+
+assert is_canada
