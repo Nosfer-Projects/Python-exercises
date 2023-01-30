365ad155477e94a70e0cef0c10cd76904032b320
Nosfer <nosfer.python@gmail.com>
2023-01-30 16:31:52 +0100

    adding other solution

--- LeetCode excercise/Move_zero.py
+++ LeetCode excercise/Move_zero.py
@@ -1,6 +1,7 @@
 #Move all zeros in unsorted array
 
 array = [0,1,0,3,12]
+#1 solve
 new_arr = []
 count = array.count(0)
 for i in array:
@@ -9,4 +10,13 @@ for i in array:
 for _ in range(count):
     new_arr.append(0)
 
-print(new_arr)
+
+#2 solve
+
+for i in array:
+    if i == 0:
+        idx = array.index(i)
+        array.pop(idx)
+        array.append(i)
+
+print(array)
