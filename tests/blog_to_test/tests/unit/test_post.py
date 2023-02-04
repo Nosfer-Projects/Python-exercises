4906c76924b70cf107e9b150c38e5f7c8a754e92
Nosfer <nosfer.python@gmail.com>
2023-02-04 20:19:45 +0100

    adding new test to test_post.py

--- tests/blog_to_test/tests/unit/test_post.py
+++ tests/blog_to_test/tests/unit/test_post.py
@@ -9,3 +9,10 @@ class PostTest(TestCase):
          
          self.assertEqual("Test", p.title)
          self.assertEqual("Test Content", p.content)
+
+    def test_json(self):
+        p = Post("Test", "Test Content")
+        excepted = {"title": "Test", "content": "Test Content"}
+
+        self.assertDictEqual(excepted, p.json())
+        
