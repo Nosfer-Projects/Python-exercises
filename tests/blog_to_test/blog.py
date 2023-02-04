b24d141b51a730c265ebc05a719491d65442584d
Nosfer <nosfer.python@gmail.com>
2023-02-04 22:13:20 +0100

    adding new method to Blog

--- tests/blog_to_test/blog.py
+++ tests/blog_to_test/blog.py
@@ -3,3 +3,11 @@ class Blog:
         self.title = title
         self.author = author
         self.posts = []
+
+    def __repr__(self):
+        pass
+    def create_post(self, title, content):
+        pass
+
+    def json(self):
+        pass
