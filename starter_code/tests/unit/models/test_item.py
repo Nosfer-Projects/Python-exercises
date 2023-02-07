153ace4f3dc88f5e81f919cfe83359856e3b8daa
Nosfer <nosfer.python@gmail.com>
2023-02-07 12:26:54 +0100

    Adding first unit tests to exercise

---
+++ starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from starter_code.models.item import ItemModel
+
+class ItemTest(TestCase):
+
+    def test_create_item(self):
+        item = ItemModel("pc", 1500)
+
+        self.assertEqual("pc", item.name)
+        self.assertEqual(1500, item.price)
+
+    def test_item_json(self):
+        pass
