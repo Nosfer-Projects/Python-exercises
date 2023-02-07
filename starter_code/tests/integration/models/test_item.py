371d1452db566b38576020b6d8645bb0d0f6c46f
Nosfer <nosfer.python@gmail.com>
2023-02-07 14:08:09 +0100

    adding code

--- starter_code/tests/integration/models/test_item.py
+++ starter_code/tests/integration/models/test_item.py
@@ -5,5 +5,6 @@ class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
             item = ItemModel("test", 19.99)
+            self.assertIsNone(ItemModel.find_by_name("test"))
             item.save_to_db()
             self.assertIsNotNone(ItemModel.find_by_name("test"))
